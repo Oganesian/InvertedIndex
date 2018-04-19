95f94284a7f43d65eceb39023e15323e73b8ffab
Karen <bezimeninofamily@gmail.com>
2018-04-19 11:33:02 +0300

    MyInvertedIndex [class] implemented

--- InvertedIndex/MyInvertedIndex.cs
+++ InvertedIndex/MyInvertedIndex.cs
@@ -4,107 +4,160 @@ namespace InvertedIndex
 {
     class MyInvertedIndex
     {
-        private List<string> searchQueriesList
-        {
-            get { return searchQueriesList; }
-            set
-            {
-                searchQueriesList = value;
-                searchQueries = null;
-            }
-        }
+        private List<List<int>> invertedIndexes = new List<List<int>>();
 
-        private List<string> searchSources
-        {
-            get { return searchSources; }
-            set
-            {
-                searchSources = value;
-                searchSource = null;
-            }
-        }
-
-        private string searchQueries
-        {
-            get { return searchQueries; }
-            set
-            {
-                searchQueries = value;
-                searchQueriesList = null;
-            }
-        }
-
-        private string searchSource
-        {
-            get { return searchSource; }
-            set
-            {
-                searchSource = value;
-                searchSources = null;
-            }
-        }
+        private List<string> searchQueries = new List<string>();
+        private List<string> searchSources = new List<string>();
 
         /// <summary>
         /// Initializes an InvertedIndex object
         /// </summary>
-        /// <param name="searchQueriesList">List of search queries</param>
+        /// <param name="searchQueries">List of search queries</param>
         /// <param name="searchSources">List of strings to search in</param>
-        public MyInvertedIndex(List<string> searchQueriesList, List<string> searchSources)
-        {
-            this.searchQueriesList = searchQueriesList;
-            this.searchSources = searchSources;
-        }
-
-        /// <summary>
-        /// Initializes an InvertedIndex object
-        /// </summary>
-        /// <param name="searchQueries"></param>
-        /// <param name="searchSource"></param>
-        public MyInvertedIndex(string searchQueries, string searchSource)
-        {
-            this.searchQueries = searchQueries;
-            this.searchSource = searchSource;
-        }
-
-        /// <summary>
-        /// Initializes an InvertedIndex object
-        /// </summary>
-        /// <param name="searchQueriesList"></param>
-        /// <param name="searchSource"></param>
-        public MyInvertedIndex(List<string> searchQueriesList, string searchSource)
-        {
-            this.searchQueriesList = searchQueriesList;
-            this.searchSource = searchSource;
-        }
-
-        /// <summary>
-        /// Initializes an InvertedIndex object
-        /// </summary>
-        /// <param name="searchQueries"></param>
-        /// <param name="searchSources"></param>
-        public MyInvertedIndex(string searchQueries, List<string> searchSources)
+        public MyInvertedIndex(List<string> searchQueries, List<string> searchSources)
         {
             this.searchQueries = searchQueries;
             this.searchSources = searchSources;
+
+            FindInvertedIndexes();
+        }
+
+        //==================================//
+        //           CONSTRUCTORS           //
+        //==================================//
+
+        /// <summary>
+        /// Initializes an InvertedIndex object
+        /// </summary>
+        /// <param name="searchQuery">Search query</param>
+        /// <param name="searchSource">String to search in</param>
+        public MyInvertedIndex(string searchQuery, string searchSource)
+        {
+            searchQueries = new List<string>();
+            searchQueries.Add(searchQuery);
+
+            searchSources = new List<string>();
+            searchSources.Add(searchSource);
+
+            FindInvertedIndexes();
+        }
+        /// <summary>
+        /// Initializes an InvertedIndex object
+        /// </summary>
+        /// <param name="searchQueries">List of search queries</param>
+        /// <param name="searchSource">String to search in</param>
+        public MyInvertedIndex(List<string> searchQueries, string searchSource)
+        {
+            this.searchQueries = searchQueries;
+
+            searchSources = new List<string>();
+            searchSources.Add(searchSource);
+
+            FindInvertedIndexes();
+        }
+
+        /// <summary>
+        /// Initializes an InvertedIndex object
+        /// </summary>
+        /// <param name="searchQuery">Search query</param>
+        /// <param name="searchSources">> of strings to search in</param>
+        public MyInvertedIndex(string searchQuery, List<string> searchSources)
+        {
+            searchQueries = new List<string>();
+            searchQueries.Add(searchQuery);
+
+            this.searchSources = searchSources;
+
+            FindInvertedIndexes();
+        }
+
+        //==================================//
+        //      GETTERS AND SETTERS         //
+        //==================================//
+
+        public void setSearchQueries(List<string> searchQueries)
+        {
+            this.searchQueries = searchQueries;
+        }
+
+        public List<string> getSearchQueries()
+        {
+            return searchQueries;
+        }
+
+        public void setSearchSources(List<string> searchSources)
+        {
+            this.searchSources = searchSources;
+        }
+
+        public List<string> getSearchSources()
+        {
+            return searchSources;
+        }
+
+        public void setSearchQuery(string searchQuery)
+        {
+            searchQueries = new List<string>();
+            searchQueries.Add(searchQuery);
+        }
+
+        public void setSearchSource(string searchSource)
+        {
+            searchSources = new List<string>();
+            searchSources.Add(searchSource);
+        }
+
+        public string getSearchQuery()
+        {
+            return searchQueries[0];
+        }
+
+        public string getSearchSource()
+        {
+            return searchSources[0];
+        }
+
+        //==================================//
+        //             METHODS              //
+        //==================================//
+
+        /// <summary>
+        /// Performs an inverted index search on the previously specified search string(-s) 
+        /// and the source string(-s)
+        /// </summary>
+        private void FindInvertedIndexes()
+        {
+            int queryIndex = 0;
+            int sourceIndex = 0;
+            foreach(string sQuery in searchQueries)
+            {
+                invertedIndexes.Add(new List<int>());
+                foreach(string sSource in searchSources)
+                {
+                    if (sSource.Contains(sQuery))
+                    {
+                        invertedIndexes[queryIndex].Add(sourceIndex);
+                    }
+                    sourceIndex++;
+                }
+                queryIndex++;
+            }
         }
 
         public override string ToString()
         {
-            string toString = "searchQueriesList: ";
-
-            if(searchQueriesList == null)
-                toString += "null";
-            else
-                foreach (string str in searchQueriesList) toString += str + " ";
-
-            toString += "\nsearchSources: ";
-            if (searchSources == null)
-                toString += "null";
-            else
-                foreach (string str in searchSources) toString += str + " ";
-
-            toString += "\nsearchQueries: " + searchQueries;
-            toString += "\nsearchSource: " + searchSource + "\n";
+            string toString = "";
+            int sourceIndex = 0;
+            foreach(string query in searchQueries)
+            {
+                toString += "\"" + query + "\"\t{ ";
+                foreach (int invertedIndex in invertedIndexes[sourceIndex])
+                {
+                    toString += invertedIndex + ", ";
+                }
+                toString = toString.Remove(toString.Length - 2);
+                toString += " }";
+            }
             return toString;
         }
     }
